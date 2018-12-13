7326fc0ed1a911ccfcc4b179579aa4f5f8aaa357
Pascal Bodin <pascal@ackl.io>
2018-12-13 08:26:00 +0100

    filter on Future result

--- TestFuture01.scala
+++ TestFuture01.scala
@@ -23,24 +23,25 @@ import scala.util.Random
 
 /**
   * This code sample demonstrates how to start several asynchronous tasks using Future,
-  * and how to immediately return a Future that will be completed by one of the three
+  * and how to immediately return a Future that will be completed with one of two
   * possible results:
-  * - the result from the first task to end (task duration)
-  * - an error status if all tasks fail (-1)
+  * - the result from the first task to end with success (task duration)
   * - an error status if no task ends before a given timeout (-2)
   *
   * A task failure is signalled by an exception. The exception is intercepted and transformed
-  * into the associated error status.
+  * into an error status.
   *
-  * In this exemple, the body of every task is a wait operation, for a random duration
-  * between 0 and maxVal milliseconds. After the wait operation, the task checks the duration.
-  * If it has been more than maxVal / 2, it triggers a RuntimeException, otherwise it
+  * Additionally, the code shows how to filter out a Future which does not satisfy
+  * a condition: check the foreach applied to every created Future.
+  *
+  * In this exemple, the body of every task is a wait operation, for a random duration  * between 0 and maxVal milliseconds. After the wait operation, the task checks the duration.
+  * If it has been more than maxVal / 4, it triggers a RuntimeException, otherwise it
   * returns the duration value and task name.
   *
   * As the wait operation is blocking, we can easily reach the maximum number of concurrent
   * threads created by the default execution context. To prevent this, the blocking function
   * is used.
-  * 
+  *
   * How to play with this sample code:
   * - to get a timeout, set maxDuration to a small value
   * - to get a task failure, hard code a long duration for one of the tasks
@@ -50,10 +51,10 @@ object TestFuture01 {
   def main(args: Array[String]): Unit = {
 
     val maxVal = 30000
-    val maxDuration = 32000  // ms
+    val maxDuration = 32000 // ms
 
     // Task names.
-    val nameArray = scala.collection.mutable.ArrayBuffer("task1", "task2", "task3", "task4")
+    val nameArray = List("task1", "task2", "task3", "task4")
 
     /**
       *
@@ -65,16 +66,18 @@ object TestFuture01 {
       println(o + " about to sleep for " + d)
       Thread.sleep(d)
       println("end of sleep for " + o)
-      if (d > maxVal / 2)
-        throw new RuntimeException("value too large")
+      if (d > maxVal / 4) {
+        println(s"throwing exception for $o")
+        throw new RuntimeException(s"value too large for $o")
+      }
       else
         (d, o)
     }
 
     /**
-      * 
-      * The transformation that converts the exception into the associated error status.
-      * 
+      *
+      * The transformation that converts the exception into an error status.
+      *
       */
     def recovery: PartialFunction[Throwable, (Int, String)] = {
       case e: RuntimeException => (-1, "")
@@ -82,7 +85,8 @@ object TestFuture01 {
 
     /**
       *
-      * Start one Future per task, and returns a Future pointing to the future result.
+      * Start one Future per task, and the timeout Future, and returns a Future that
+      * will point to an acceptable result.
       *
       */
     def getFirstSuccess: Future[(Int, String)] = {
@@ -96,11 +100,14 @@ object TestFuture01 {
           blocking {
             taskComputation(name)
           }
-        } recover recovery foreach synchroP.trySuccess
-        // The above line is rewritten by Scala as:
-        // .recover(recovery).foreach(r => synchroP.trySuccess(r))
+        } // Transform possible exception into (-1, "")
+          .recover(recovery)
+          // Make the Promise succeed only if Future result is what we are waiting for.
+          .foreach({ case (status, name) =>
+          if (status > 0) synchroP.trySuccess((status, name))
+        })
       }
-      
+
       // Timeout.
       Future {
         blocking {
@@ -120,11 +127,11 @@ object TestFuture01 {
     // Display number of available processors.
     val runtime = Runtime.getRuntime
     println("available processors: " + runtime.availableProcessors)
-    
+
     // Let's go!
     println("waiting for result...")
     val r = Await.result(getFirstSuccess, Duration.Inf)
     println("result is: " + r._1 + " - " + r._2)
-    
+
   }
 }
